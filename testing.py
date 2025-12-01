b2ebf11f651f9bdc196105cfc716815ecb2d2610
Satyajit-1219 <mulaysatyajit1219@gmail.com>
2025-12-01 15:38:31 +0530

    Refactor multiplication logic with user input
    Comment out the mult function and its call, and add user input for multiplication.

--- testing.py
+++ testing.py
@@ -1,7 +1,13 @@
 print("Hello World")
 print("Satyajit Shivaji Mulay")
 print("Rohan Sadashiv Bhujbal")
-def mult(a,b):
-    return a*b
+#def mult(a,b):
+    #return a*b
 
-print(mult(4,5))
+#print(mult(4,5))
+a = int(input("Enter a number"))
+b = int(input("Enter another number "))
+c = a*b
+print("Multiplication of the numbers is: ",c)
+
+
